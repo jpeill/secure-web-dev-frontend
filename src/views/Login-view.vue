7fac74dffc50bc9e5d2df641e9c50beec6261288
jpeill <peilleron.jeremy@gmail.com>
2023-02-05 21:40:33 +0100

    setup vue

---
+++ src/views/Login-view.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <h1>LOGIN</h1>
+      <form @submit.prevent="login">
+        <input v-model="username" placeholder="username" />
+        <br />
+        <br />
+        <input v-model="password" placeholder="password" type="password" />
+        <br />
+        <br />
+        <button type="submit">Connexion</button>
+        <br />
+        <br />
+      </form>
+      <router-link to="/register"><button>S'inscrire</button></router-link>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapState, mapActions } from 'vuex';
+export default {
+  computed: {
+    ...mapState({
+      token: (state) => state.token,
+    }),
+  },
+  methods: {
+    ...mapActions(['setToken']),
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:3000/users/login', {
+          username: this.username,
+          password: this.password,
+        });
+        this.setToken(response.data.jwt);
+      } catch (error) {
+        console.error(error);
+      }
+      this.$router.push("/");
+    },
+    
+  },
+
+};
+
+</script>
---
+++ src/views/Welcome-view.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Bienvenue
+    <router-link v-if="!isLoggedIn" to="/login"><button>Afficher les locations</button></router-link>
+    <router-link v-if="isLoggedIn" to="/locations"><button>Afficher les locations</button></router-link>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["isLoggedIn"]),
+  }
+};
+
+</script>
